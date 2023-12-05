6930e47cbecbfb5fdfbded8473a518dc0d7d2502
alhendi <al-hendi@publicplan.de>
2023-12-05 14:12:42 +0100

    neue Verbindung-DB

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux'
 import { ActionType } from '../state/action-types'
 import { useState } from 'react'
 import axios from 'axios'
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 type Props = {}
 
@@ -16,6 +17,7 @@ const Login = () => {
   const dispatch = useDispatch()
 
   const loginUser = async () => {
+    setIsLoading(true)
     dispatch({ type: ActionType.LOGIN_USER_BEGIN })
 
     try {
@@ -47,39 +49,79 @@ const Login = () => {
     } catch (error: any) {
       setIsLoading(false)
       setAlert({ type: 'danger', text: error.response.data.msg })
-      dispatch({
-        type: ActionType.LOGIN_USER_ERROR,
-      })
+
     }
   }
 
   return (
-    <div style={{ marginTop: '200px' }}>
-      <div>
-        <h2>Login</h2>
-        {alert && (
-          <div className={`alert alert-${alert.type}`} role='alert'>
-            {alert.text}
-          </div>
-        )}
-      </div>
+    <>
+    {alert && (
+        <div className={`alert alert-${alert.type}`} role="alert">
+          {alert.text}
+        </div>
+      )}
 
-      <input
-        type='text'
-        placeholder='Benutzername'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type='password'
-        placeholder='Passwort'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={loginUser} disabled={isLoading}>
-        {isLoading ? 'login läuft...' : 'login'}
-      </button>
-    </div>
+     <Container>
+       <Row className="mt-5 pt-5  d-flex justify-content-center align-items-center">
+         <Col md={8} lg={6} xs={12}>
+           <div className="border border-3 border-primary"></div>
+           <Card className="shadow">
+             <Card.Body>
+               <div className="mb-3 mt-md-4">
+                 <h2 className="fw-bold mb-2 text-uppercase ">Transport-Deutschland</h2>
+                 <p className=" mb-5">Bitte geben Sie Ihr Login und Passwort ein!
+</p>
+                 <div className="mb-3">
+                   <Form>
+                     <Form.Group className="mb-3" controlId="formBasicEmail">
+                       <Form.Label className="text-center">
+                         Email address
+                       </Form.Label>
+                       <Form.Control type="email" placeholder="E-Mail eingeben"  value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+                     </Form.Group>
+
+                     <Form.Group
+                       className="mb-3"
+                       controlId="formBasicPassword"
+                     >
+                       <Form.Label>Passwort</Form.Label>
+                       <Form.Control type="password" placeholder="Passwort" value={password}
+        onChange={(e) => setPassword(e.target.value)} />
+                     </Form.Group>
+                     <Form.Group
+                       className="mb-3"
+                       controlId="formBasicCheckbox"
+                     >
+                       <p className="small">
+                         <a className="text-primary" href="#!">
+                         Passwort vergessen?
+                         </a>
+                       </p>
+                     </Form.Group>
+                     <div className="d-grid">
+                       <Button variant="primary" type="submit" onClick={loginUser} disabled={isLoading}>
+                       {isLoading ? 'login läuft...' : 'login'}
+                       </Button>
+                     </div>
+                   </Form>
+                   <div className="mt-3">
+                     <p className="mb-0  text-center">
+                     Sie haben noch kein Konto?
+                       <a href="register" className="text-primary fw-bold">
+                       Registrieren
+                       </a>
+                     </p>
+                   </div>
+                 </div>
+               </div>
+             </Card.Body>
+           </Card>
+         </Col>
+       </Row>
+     </Container>
+  
+   </>
   )
 }
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,125 +1,194 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import axios from 'axios'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 const Register: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [alert, setAlert] = useState<{ type: string; text: string } | null>(
     null
-  )
+  );
 
   interface data {
-    email: string
-    password: string
-    firstname: string
-    lastname: string
-    type: string
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    type: string;
   }
   interface ApiResponse {
     user: {
-      email: string
-      password: string
-      firstname: string
-      lastname: string
-      type: string
-    }
-    token: string
+      email: string;
+      password: string;
+      firstname: string;
+      lastname: string;
+      type: string;
+    };
+    token: string;
     // Add more properties as per your API response structure
   }
 
   const initialState: data = {
-    email: '',
-    password: '',
-    firstname: '',
-    lastname: '',
-    type: 'privatkunde',
-  }
-  const [values, setValues] = useState(initialState)
+    email: "",
+    password: "",
+    firstname: "",
+    lastname: "",
+    type: "privatkunde",
+  };
+  const [values, setValues] = useState(initialState);
 
   const handleChange = (e: any) => {
-    console.log(e.target.value)
-    setValues({ ...values, [e.target.name]: e.target.value })
-  }
-  const dispatch = useDispatch()
+    console.log(e.target.value);
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const dispatch = useDispatch();
 
   const handleRegister = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     try {
       const response = await axios.post<ApiResponse>(
-        'http://127.0.0.1:5000/api/v1/auth/register',
+        "http://127.0.0.1:5000/api/v1/auth/register",
         values
-      )
-      const data: ApiResponse = response.data
-      console.log(data)
+      );
+      const data: ApiResponse = response.data;
+      console.log(data);
       if (response.status === 201) {
-        setIsLoading(false)
-        setAlert({ type: 'success', text: 'Registrierung erfolgreich' })
+        setIsLoading(false);
+        setAlert({ type: "success", text: "Registrierung erfolgreich" });
 
         // Hier kannst du den Erfolg an deinen Redux-Store weitergeben
         dispatch({
-          type: 'REGISTER_USER_SUCCESS',
+          type: "REGISTER_USER_SUCCESS",
           payload: {
             username: data.user.firstname,
             token: data.token,
-            msg: 'Registrierung erfolgreich',
+            msg: "Registrierung erfolgreich",
           },
-        })
+        });
       } else {
-        setIsLoading(false)
-        setAlert({ type: 'danger', text: 'Registrierung fehlgeschlagen' })
+        setIsLoading(false);
+        setAlert({ type: "danger", text: "Registrierung fehlgeschlagen" });
       }
     } catch (error: any) {
-      setIsLoading(false)
-      setAlert({ type: 'danger', text: error.response.data.msg })
+      setIsLoading(false);
+      setAlert({ type: "danger", text: error.response.data.msg });
     }
-  }
+  };
 
   return (
     <div>
-      <h2>Registrierung</h2>
       {alert && (
-        <div className={`alert alert-${alert.type}`} role='alert'>
+        <div className={`alert alert-${alert.type}`} role="alert">
           {alert.text}
         </div>
       )}
-      <input
-        type='text'
-        placeholder='lastname'
-        value={values.lastname}
-        onChange={handleChange}
-        name='lastname'
-      />
-      <input
-        type='text'
-        placeholder='firstname'
-        value={values.firstname}
-        onChange={handleChange}
-        name='firstname'
-      />
-      <input
-        type='email'
-        placeholder='email'
-        value={values.email}
-        onChange={handleChange}
-        name='email'
-      />
-      <input
-        type='password'
-        placeholder='Passwort'
-        value={values.password}
-        name='password'
-        onChange={handleChange}
-      />
-      <select name='type' id='cars' onChange={handleChange}>
-        <option value='privatkunde'>privatkunde</option>
-        <option value='geschäftskunde'>geschäftskunde</option>
-      </select>
-      <button onClick={handleRegister} disabled={isLoading}>
-        {isLoading ? 'Registrierung läuft...' : 'Registrieren'}
-      </button>
-    </div>
-  )
-}
 
-export default Register
+
+      <Container>
+        <Row className=" mt-5 pt-5 d-flex justify-content-center align-items-center">
+          <Col md={8} lg={6} xs={12}>
+            <div className="border border-3 border-primary"></div>
+            <Card className="shadow">
+              <Card.Body>
+                <div className="mb-3 mt-md-4">
+                  <h2 className="fw-bold mb-2 text-uppercase ">
+                    Transport-Deutschland
+                  </h2>
+                  <p className=" mb-5">
+                    Bitte geben Sie Ihr Login und Passwort ein!
+                  </p>
+                  <div className="mb-3">
+                    <Form>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">Name</Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Name"
+                          value={values.firstname}
+                          onChange={handleChange}
+                          name="firstname"
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Nachname
+                        </Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="Nachname"
+                          value={values.lastname}
+                          onChange={handleChange}
+                          name="lastname"
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label className="text-center">
+                          Email address
+                        </Form.Label>
+                        <Form.Control
+                          type="email"
+                          placeholder="E-Mail eingeben"
+                          value={values.email}
+                          onChange={handleChange}
+                          name="email"
+                        />
+                      </Form.Group>
+
+                      <Form.Group
+                        className="mb-3"
+                        controlId="formBasicPassword"
+                      >
+                        <Form.Label>Passwort</Form.Label>
+                        <Form.Control
+                          type="password"
+                          placeholder="Passwort"
+                          value={values.password}
+                          name="password"
+                          onChange={handleChange}
+                        />
+                      </Form.Group>
+                      <Form.Select
+                        aria-label="Default select example"
+                        id="cars"
+                        onChange={handleChange}
+                        className="mb-3"
+                      >
+                        <option>Öffnen Sie dieses Auswahlmenü</option>
+                        <option value="privatkunde">privatkunde</option>
+                        <option value="geschäftskunde">geschäftskunde</option>
+                      </Form.Select>
+
+                      <div className="d-grid">
+                        <Button
+                          variant="primary"
+                          type="submit"
+                          onClick={handleRegister}
+                          disabled={isLoading}
+                        >
+                          {isLoading
+                            ? "Registrierung läuft..."
+                            : "Registrieren"}
+                        </Button>
+                      </div>
+                    </Form>
+                    <div className="mt-3">
+                      <p className="mb-0  text-center">
+                      Sie haben bereits ein Konto?
+                        <a href="login" className="text-primary fw-bold">
+                        Anmelden
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Register;
